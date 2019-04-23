7911f8f64e0680e6aeb9a2ef7cc38a298e2e078c
sunyunfei <1035044809@qq.com>
2019-04-23 14:42:08 +0800

    first

---
+++ Record/Record/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Record
+//
+//  Created by 孙云飞 on 2019/4/23.
+//  Copyright © 2019 syf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
